f069664eaccac14f451301c6637f2ebf5f90720e
u2 <u2@lenta.ru>
2020-12-16 20:23:35 +0500

    consistenthash.go hbm

--- Lesson6/consistenthash/consistenthash.go
+++ Lesson6/consistenthash/consistenthash.go
@@ -1,11 +1,16 @@
 package consistenthash
 
+//NewRing func
 // func NewRing() *Ring
 
+//AddNode func
 // func (r *Ring) AddNode(id string)
 
+//RemoveNode func
 // func (r *Ring) RemoveNode(id string) error
 
+//NodeByID func
 // func (r *Ring) NodeByID(key string) string
 
+//
 //В процессе реализации
