7611db1564fe6a806d150320997369521836a0e3
u2 <u2@lenta.ru>
2020-12-17 17:26:29 +0500

    consistenthash.go has been modified

--- Lesson6/consistenthash/consistenthash.go
+++ Lesson6/consistenthash/consistenthash.go
@@ -1,16 +1,82 @@
 package consistenthash
 
-//NewRing func
-// func NewRing() *Ring
+import (
+	"errors"
+	"hash/crc32"
+	"sort"
+	"sync"
+)
 
-//AddNode func
-// func (r *Ring) AddNode(id string)
+var ErrNodeNotFound = errors.New("node not found")
 
-//RemoveNode func
-// func (r *Ring) RemoveNode(id string) error
+type Ring struct {
+	Nodes Nodes
+	sync.Mutex
+}
 
-//NodeByID func
-// func (r *Ring) NodeByID(key string) string
+func NewRing() *Ring {
+	return &Ring{Nodes: Nodes{}}
+}
 
-//
-//В процессе реализации
+func (r *Ring) AddNode(id string) {
+	r.Lock()
+	defer r.Unlock()
+
+	node := NewNode(id)
+	r.Nodes = append(r.Nodes, node)
+
+	sort.Sort(r.Nodes)
+}
+
+func (r *Ring) RemoveNode(id string) error {
+	r.Lock()
+	defer r.Unlock()
+
+	i := r.search(id)
+	if i >= r.Nodes.Len() || r.Nodes[i].Id != id {
+		return ErrNodeNotFound
+	}
+
+	r.Nodes = append(r.Nodes[:i], r.Nodes[i+1:]...)
+
+	return nil
+}
+
+func (r *Ring) Get(id string) string {
+	i := r.search(id)
+	if i >= r.Nodes.Len() {
+		i = 0
+	}
+
+	return r.Nodes[i].Id
+}
+
+func (r *Ring) search(id string) int {
+	searchfn := func(i int) bool {
+		return r.Nodes[i].HashId >= hashId(id)
+	}
+
+	return sort.Search(r.Nodes.Len(), searchfn)
+}
+
+type Node struct {
+	Id     string
+	HashId uint32
+}
+
+func NewNode(id string) *Node {
+	return &Node{
+		Id:     id,
+		HashId: hashId(id),
+	}
+}
+
+type Nodes []*Node
+
+func (n Nodes) Len() int           { return len(n) }
+func (n Nodes) Swap(i, j int)      { n[i], n[j] = n[j], n[i] }
+func (n Nodes) Less(i, j int) bool { return n[i].HashId < n[j].HashId }
+
+func hashId(key string) uint32 {
+	return crc32.ChecksumIEEE([]byte(key))
+}
