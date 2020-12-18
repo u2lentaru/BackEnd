40eb7a70f715300e1910f525a5924087a41c363f
u2 <u2@lenta.ru>
2020-12-18 12:20:46 +0500

    consistenthash.go hbm

--- Lesson6/consistenthash/consistenthash.go
+++ Lesson6/consistenthash/consistenthash.go
@@ -7,17 +7,21 @@ import (
 	"sync"
 )
 
+//ErrNodeNotFound - node not found
 var ErrNodeNotFound = errors.New("node not found")
 
+//Ring struct
 type Ring struct {
 	Nodes Nodes
 	sync.Mutex
 }
 
+//NewRing func
 func NewRing() *Ring {
 	return &Ring{Nodes: Nodes{}}
 }
 
+//AddNode func
 func (r *Ring) AddNode(id string) {
 	r.Lock()
 	defer r.Unlock()
@@ -28,12 +32,13 @@ func (r *Ring) AddNode(id string) {
 	sort.Sort(r.Nodes)
 }
 
+//RemoveNode func
 func (r *Ring) RemoveNode(id string) error {
 	r.Lock()
 	defer r.Unlock()
 
 	i := r.search(id)
-	if i >= r.Nodes.Len() || r.Nodes[i].Id != id {
+	if i >= r.Nodes.Len() || r.Nodes[i].ID != id {
 		return ErrNodeNotFound
 	}
 
@@ -42,41 +47,45 @@ func (r *Ring) RemoveNode(id string) error {
 	return nil
 }
 
+//Get func
 func (r *Ring) Get(id string) string {
 	i := r.search(id)
 	if i >= r.Nodes.Len() {
 		i = 0
 	}
 
-	return r.Nodes[i].Id
+	return r.Nodes[i].ID
 }
 
 func (r *Ring) search(id string) int {
 	searchfn := func(i int) bool {
-		return r.Nodes[i].HashId >= hashId(id)
+		return r.Nodes[i].HashID >= hashID(id)
 	}
 
 	return sort.Search(r.Nodes.Len(), searchfn)
 }
 
+//Node struct
 type Node struct {
-	Id     string
-	HashId uint32
+	ID     string
+	HashID uint32
 }
 
+//NewNode func
 func NewNode(id string) *Node {
 	return &Node{
-		Id:     id,
-		HashId: hashId(id),
+		ID:     id,
+		HashID: hashID(id),
 	}
 }
 
+//Nodes type
 type Nodes []*Node
 
 func (n Nodes) Len() int           { return len(n) }
 func (n Nodes) Swap(i, j int)      { n[i], n[j] = n[j], n[i] }
-func (n Nodes) Less(i, j int) bool { return n[i].HashId < n[j].HashId }
+func (n Nodes) Less(i, j int) bool { return n[i].HashID < n[j].HashID }
 
-func hashId(key string) uint32 {
+func hashID(key string) uint32 {
 	return crc32.ChecksumIEEE([]byte(key))
 }
