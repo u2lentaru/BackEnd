a07ce321f87f772dd75ce2e08b53c0a5447acb89
u2 <u2@lenta.ru>
2020-12-19 16:55:12 +0500

    sessions added

---
+++ Lesson7/sessions/client.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-redis/redis/v8"
+)
+
+//RedisClient struct
+type RedisClient struct {
+	*redis.Client
+}
+
+//NewRedisClient func
+func NewRedisClient(host, port string) (*RedisClient, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", host, port),
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	err := client.Ping(context.Background()).Err()
+	if err != nil {
+		return nil, fmt.Errorf("try to ping to redis: %w", err)
+	}
+
+	c := &RedisClient{
+		Client: client,
+	}
+
+	return c, nil
+}
+
+//Close func
+func (c *RedisClient) Close() error {
+	return c.Client.Close()
+}
+
+//GetRecord func
+func (c *RedisClient) GetRecord(mkey string) ([]byte, error) {
+	data, err := c.Get(context.Background(), mkey).Bytes()
+	if err == redis.Nil {
+		// we got empty result, it's not an error
+		return nil, nil
+	} else if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
