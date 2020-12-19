19085def0dfe0b1dd4bc3f268bf810d702552263
u2 <u2@lenta.ru>
2020-12-19 16:50:09 +0500

    main.go hbm

--- Lesson7/main.go
+++ Lesson7/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"time"
 )
 
-//docker run -p ​6379​:​6379​  -d redis
+//docker run -p 6379:6379  -d redis
 
 func basicWork(client *RedisClient) error {
 	const (
@@ -97,6 +98,51 @@ func basicWork(client *RedisClient) error {
 	return nil
 }
 
+func withStructWork(client *RedisClient) error {
+	jsonKey := "json_key"
+	// comment it if you want data from previous launch
+	/**/
+	err := client.Del(context.Background(), []string{jsonKey}...).Err()
+	if err != nil {
+		return err
+	}
+	/**/
+
+	type exampleStruct struct {
+		FieldOne string `json:"field_one"`
+		FieldTwo string `json:"field_two"`
+	}
+
+	s := exampleStruct{
+		FieldOne: "one",
+		FieldTwo: "two",
+	}
+
+	data, err := json.Marshal(s)
+	if err != nil {
+		return err
+	}
+
+	ttl := 5 * time.Second
+	err = client.Set(context.Background(), jsonKey, data, ttl).Err()
+	if err != nil {
+		return err
+	}
+	item, err := client.GetRecord(jsonKey)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("GetRecord for key %q `%s`\n", jsonKey, item)
+
+	e := new(exampleStruct)
+	if err := json.Unmarshal(item, e); err != nil {
+		return err
+	}
+	fmt.Printf("example struct: %+v\n", e)
+
+	return nil
+}
+
 func main() {
 	const (
 		host = "localhost"
@@ -109,7 +155,8 @@ func main() {
 	}
 	defer client.Close()
 
-	if err := basicWork(client); err != nil {
+	// if err := basicWork(client); err != nil {
+	if err := withStructWork(client); err != nil {
 		log.Fatal(err)
 	}
 }
